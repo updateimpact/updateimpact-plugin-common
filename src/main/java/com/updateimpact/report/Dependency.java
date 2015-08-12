8c2b4c13e96a8ffc0321e5912db7abfea9634d80
adamw <adam@warski.org>
2015-08-12 12:07:33 +0200

    Release 1.3.0: new model

--- src/main/java/com/updateimpact/report/Dependency.java
+++ src/main/java/com/updateimpact/report/Dependency.java
@@ -4,19 +4,31 @@ import java.util.List;
 
 public class Dependency {
     private final DependencyId id;
-    private final List<DependencyChild> dependencies;
+    private final String evictedByVersion;
+    private final Boolean cycle;
+    private final List<DependencyId> children;
 
-    public Dependency(DependencyId id, List<DependencyChild> dependencies) {
+    public Dependency(DependencyId id, String evictedByVersion, Boolean cycle, List<DependencyId> children) {
         this.id = id;
-        this.dependencies = dependencies;
+        this.evictedByVersion = evictedByVersion;
+        this.cycle = cycle;
+        this.children = children;
     }
 
     public DependencyId getId() {
         return id;
     }
 
-    public List<DependencyChild> getDependencies() {
-        return dependencies;
+    public String getEvictedByVersion() {
+        return evictedByVersion;
+    }
+
+    public Boolean getCycle() {
+        return cycle;
+    }
+
+    public List<DependencyId> getChildren() {
+        return children;
     }
 
     @Override
@@ -27,14 +39,19 @@ public class Dependency {
         Dependency that = (Dependency) o;
 
         if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        return !(dependencies != null ? !dependencies.equals(that.dependencies) : that.dependencies != null);
+        if (evictedByVersion != null ? !evictedByVersion.equals(that.evictedByVersion) : that.evictedByVersion != null)
+            return false;
+        if (cycle != null ? !cycle.equals(that.cycle) : that.cycle != null) return false;
+        return !(children != null ? !children.equals(that.children) : that.children != null);
 
     }
 
     @Override
     public int hashCode() {
         int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (dependencies != null ? dependencies.hashCode() : 0);
+        result = 31 * result + (evictedByVersion != null ? evictedByVersion.hashCode() : 0);
+        result = 31 * result + (cycle != null ? cycle.hashCode() : 0);
+        result = 31 * result + (children != null ? children.hashCode() : 0);
         return result;
     }
 }
